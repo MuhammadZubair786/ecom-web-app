88978b8f7e57567226c4b4d29e8912f9a97083cb
thehouseofquantumnetworks <thehouseofquantumnetworks@gmail.com>
2026-02-01 10:47:54 +0500

    update

--- user dashboard/chat.js
+++ user dashboard/chat.js
@@ -1,14 +1,22 @@
 var sendBtn = document.getElementById("sendBtn");
 var chats = document.getElementById("chats");
+var input = document.getElementById("input");
 
 var ROOMID = "";
 
-var chatRef = "";
+var chatRef = ""; // for run time chat show
 
+input.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    sendBtn.click()
+
+
+  }
+});
+
+//send message
 sendBtn.addEventListener("click", async function () {
   console.log(ROOMID);
-  chatRef = await firebase.database().ref("chats").child(ROOMID);
-  var input = document.getElementById("input");
 
   console.log(chatRef);
 
@@ -17,15 +25,50 @@ sendBtn.addEventListener("click", async function () {
   var senderid = localStorage.getItem("userUid");
   var adminid = "gOXcAXpZhYcNvVHeEerKzKD1Dat1";
 
-  var object = { senderid, adminid, messageKey, message: input.value };
+  var object = {
+    senderid: senderid,
+    RecieverId: adminid,
+    messageKey,
+    message: input.value,
+  };
   await chatRef.child(messageKey).set(object);
-  alert("send message");
+  input.value=""
+  // alert("send message");
 });
 
-async function checkRoom(senderId, RecieverId) {
-  console.log(senderId);
-  console.log(RecieverId);
+async function createRoomiD(senderId, RecieverId) {
+  var roomid = await firebase.database().ref("chatList").push().getKey();
+  var object = {
+    senderId,
+    RecieverId,
+    roomid,
+  };
 
+  await firebase.database().ref("chatList").child(roomid).set(object);
+  return roomid;
+}
+
+async function returnRoomId(senderId, RecieverId, chatlist) {
+  var roomid = "";
+  for (var i = 0; i < chatlist.length; i++) {
+    console.log(chatlist[i]);
+
+    if (
+      (chatlist[i]["RecieverId"] == RecieverId &&
+        chatlist[i]["senderId"] == senderId) ||
+      (chatlist[i]["RecieverId"] == senderId &&
+        chatlist[i]["senderId"] == RecieverId)
+    ) {
+      console.log(chatlist[i]);
+      roomid = chatlist[i]["roomid"]; //room create ho already
+      break;
+    }
+  }
+  return roomid;
+}
+
+//room check => exit=>return id,create room id ,create room id
+async function checkRoom(senderId, RecieverId) {
   await firebase
     .database()
     .ref("chatList")
@@ -33,47 +76,25 @@ async function checkRoom(senderId, RecieverId) {
     .then(async (snap) => {
       console.log(snap.val()); //chat empty
       if (snap.val() == null) {
-        var roomid = await firebase.database().ref("chatList").push().getKey();
-        var object = {
-          senderId,
-          RecieverId,
-          roomid,
-        };
-        await firebase.database().ref("chatList").child(roomid).set(object);
-
-        alert("room create succesfully");
+        var roomid = await createRoomiD(senderId, RecieverId);
+        console.log(roomid);
         ROOMID = roomid;
       } else {
         var chatlist = Object.values(snap.val());
         console.log(chatlist);
-        for (var i = 0; i < chatlist.length; i++) {
-          console.log(chatlist[i]);
-          if (
-            (chatlist[i]["RecieverId"] == RecieverId &&
-              chatlist[i]["senderId"] == senderId) ||
-            (chatlist[i]["RecieverId"] == senderId &&
-              chatlist[i]["senderId"] == RecieverId)
-          ) {
-            console.log(chatlist[i]);
-            ROOMID = chatlist[i]["roomid"];
-            break;
-          }
-        }
+        var roomid = await returnRoomId(senderId, RecieverId, chatlist);
+        ROOMID = roomid;
       }
       if (ROOMID == "") {
-        var roomid = await firebase.database().ref("chatList").push().getKey();
-        var object = {
-          senderId,
-          RecieverId,
-          roomid,
-        };
-        await firebase.database().ref("chatList").child(roomid).set(object);
+        var roomid = await createRoomiD(senderId, RecieverId);
+        console.log(roomid);
         ROOMID = roomid;
       }
     })
     .catch((e) => {
       console.log(e);
     });
+  console.log(ROOMID);
   return ROOMID;
 }
 
@@ -81,26 +102,37 @@ async function loadfunction() {
   var senderid = localStorage.getItem("userUid");
   var adminid = "gOXcAXpZhYcNvVHeEerKzKD1Dat1"; //
   var roomChat = await checkRoom(senderid, adminid); //room create
+  chatRef = await firebase.database().ref("chats").child(roomChat); // set room ref
+  console.log(chatRef);
+
   getRoomsChat(roomChat); //get all chats
 }
 
+//get all chats
 async function getRoomsChat(roomChat) {
   var senderid = localStorage.getItem("userUid");
-  await firebase
-    .database()
-    .ref("chats")
-    .child(roomChat)
-    .get()
-    .then((snap) => {
-      console.log(snap.val());
-      if (snap.val() == null) {
-        return;
-      }
-      var data = Object.values(snap.val());
-      for (var i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        if (senderid == data[i]["senderId"]) {
-          chats.innerHTML += `
+  console.log(chatRef);
+  // await firebase
+  //   .database()
+  //   .ref("chats")
+  //   .child(roomChat)
+  //   .get()
+  //   .then((snap) => {
+  chatRef.on("value", (snap) => {
+    chats.innerHTML = "";
+
+    console.log(snap.val());
+    if (snap.val() == null) {
+      return;
+    }
+    var data = Object.values(snap.val());
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+
+      if (senderid == data[i]["senderid"]) {
+        chats.innerHTML += `
+          <div class='row'> 
+          <div class='col col-lg-6'></div>
             <div class='col col-lg-6'>
             <h3 class="send">
             <b class="chatcontainer"> ${data[i]["message"]} </b>
@@ -108,22 +140,26 @@ async function getRoomsChat(roomChat) {
 
 
             </div>
+            </div>
             `;
-        }
-        else{
-              chats.innerHTML += `
-            <div class='col'>
+      } else {
+        chats.innerHTML += `
+            <div class='row'> 
+            <div class='col col-lg-6'>
             <h3 class="recieve">
             <b class="chatcontainer"> ${data[i]["message"]} </b>
           </h3>
 
 
             </div>
+          <div class='col col-lg-6'></div>
+
+            </div>
             `;
-        }
       }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+    }
+  });
+  // .catch((e) => {
+  //   console.log(e);
+  // });
 }
